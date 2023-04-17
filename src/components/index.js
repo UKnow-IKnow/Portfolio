29bd95c78e5f4be47c382127baeb0bce22bb556b
UKnow-IKnow <sarojsahu014@gmail.com>
2023-04-17 13:27:59 +0530

    working with project section

--- src/components/index.js
+++ src/components/index.js
@@ -4,7 +4,7 @@ import Navbar from './Navbar';
 import About from './About';
 import Tech from './Tech';
 import Experience from './Experience';
-import Works from './Works';
+import Projects from './Projects';
 import Recommendations from './Recommendations';
 import Contact from './Contact';
 
@@ -14,7 +14,7 @@ export {
   About,
   Tech,
   Experience,
-  Works,
+  Projects,
   Recommendations,
   Contact,
   EarthCanvas, 
